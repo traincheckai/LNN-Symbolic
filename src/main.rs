b00a63a3445b91b3ba477369a68032ae25ec0c8c
mattfaltyn <mattfaltyn@dhcp-206-12-41-224.ubcsecure.wireless.ubc.ca>
2022-09-07 20:02:05 -0700

    Added edit method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::stdin;
+
 #[derive(Debug)]
 struct Proposition {
     name: String,
@@ -7,19 +9,27 @@ struct Proposition {
 trait Leaf {
     fn new(name: String) -> Self;
     fn print(&self);
+    fn edit(&mut self);
 }
 
 impl Leaf for Proposition {
     fn new(name: String) -> Proposition {
-        return Proposition { name, value: 0.5 };
+        return Proposition { name, value: 1.0 };
     }
 
     fn print(&self) {
-        println!("{}", self.name);
+        println!("{}, {}", self.name, self.value);
+    }
+
+    fn edit(&mut self) {
+        let new_val = 0.5;
+        self.value = new_val;
     }
 }
 
 fn main() {
     let mut messi: Proposition = Leaf::new(String::from("Messi"));
     messi.print();
+    messi.edit();
+    messi.print()
 }
