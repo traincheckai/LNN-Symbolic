80b3b1c64dc1c2b1fdebc50440d8839dcbb57ee1
mattfaltyn <mattfaltyn@dhcp-206-12-41-224.ubcsecure.wireless.ubc.ca>
2022-09-07 20:04:08 -0700

    Reformat

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use std::io::stdin;
-
+// Structs
 #[derive(Debug)]
 struct Proposition {
     name: String,
     value: f64,
 }
 
+// Traits
 trait Leaf {
     fn new(name: String) -> Self;
     fn print(&self);
     fn edit(&mut self);
 }
 
+// Implementations
 impl Leaf for Proposition {
     fn new(name: String) -> Proposition {
         return Proposition { name, value: 1.0 };
